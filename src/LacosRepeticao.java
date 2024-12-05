3198983f5e2a51e7de9917ffd3aec297b5094a4e
Laura Fumagalli <laura.rodrigues@zup.com.br>
2024-12-05 09:45:58 -0300

    feat: add main with matriz and LacosRepeticao with for

--- src/LacosRepeticao.java
+++ src/LacosRepeticao.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LacosRepeticao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[7];
+        System.out.println("Digite 5 número:");
+
+        for(int index = 0; index < numeros.length; index++) {
+            System.out.println("Número da posição " + index + ":");
+            numeros[index] = scanner.nextInt();
+        }
+        scanner.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] listaDeCompras = {"Arroz", "Feijão", "Leite", "Pão", "Ovos"};
+        System.out.println(listaDeCompras[0]);
+        System.out.println(listaDeCompras[4]);
+//        System.out.println(listaDeCompras[5]);
+//        Index 5 out of bounds for length 5
+//        at Main.main(Main.java:6)
+
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(matriz[1][1]);
+
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        System.out.println(numeros[0]);
+        numeros[0] = scanner.nextInt();
+        System.out.println(numeros[0]);
     }
 }
